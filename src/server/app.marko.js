05e4b01f816325dc66f5440eaad3c67836edd1ab
Ryan Balla <rballa@signal.co>
2019-07-15 11:16:22 -0500

    marko

---
+++ src/server/app.marko.js
@@ -0,0 +1,45 @@
+// Compiled using marko@4.18.9 - DO NOT EDIT
+"use strict";
+
+var marko_template = module.exports = require("marko/src/html").t(__filename),
+    marko_componentType = "/Roundhouse$1.0.0/src/server/app.marko",
+    components_helpers = require("marko/src/runtime/components/helpers"),
+    marko_renderer = components_helpers.r,
+    marko_defineComponent = components_helpers.c,
+    marko_helpers = require("marko/src/runtime/html/helpers"),
+    marko_loadTag = marko_helpers.t,
+    component_globals_tag = marko_loadTag(require("marko/src/core-tags/components/component-globals-tag")),
+    init_components_tag = marko_loadTag(require("marko/src/core-tags/components/init-components-tag")),
+    await_reorderer_tag = marko_loadTag(require("marko/src/core-tags/core/await/reorderer-renderer"));
+
+function render(input, out, __component, component, state) {
+  var data = input;
+
+  out.w("<!doctype html><html><head><title>Hello World</title></head><body>");
+
+  component_globals_tag({}, out);
+
+  out.w("<h1>Hello World</h1>");
+
+  init_components_tag({}, out);
+
+  await_reorderer_tag({}, out, __component, "5");
+
+  out.w("</body></html>");
+}
+
+marko_template._ = marko_renderer(render, {
+    ___implicit: true,
+    ___type: marko_componentType
+  });
+
+marko_template.Component = marko_defineComponent({}, marko_template._);
+
+marko_template.meta = {
+    id: "/Roundhouse$1.0.0/src/server/app.marko",
+    tags: [
+      "marko/src/core-tags/components/component-globals-tag",
+      "marko/src/core-tags/components/init-components-tag",
+      "marko/src/core-tags/core/await/reorderer-renderer"
+    ]
+  };
--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('marko/node-require');
 
 const express = require('express');
 const api = require('./api');
@@ -10,23 +11,93 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../knexfile')[environment];
 const database = require('knex')(configuration);
 const cloudinary = require('cloudinary');
+const markoExpress = require('marko/express');
+
+const createError = require('http-errors');
+const logger = require('morgan');
+const session = require('express-session');
+const okta = require('@okta/okta-sdk-nodejs');
+const ExpressOIDC = require('@okta/oidc-middleware').ExpressOIDC;
+
+const oktaClient = new okta.Client({
+  orgUrl: process.env.OKTA_ORG,
+  token: process.env.OKTA_TOKEN_VALUE,
+});
+const oidc = new ExpressOIDC({
+  issuer: `${process.env.OKTA_ORG}/oauth2/default`,
+  client_id: process.env.OKTA_CLIENT_ID,
+  client_secret: process.env.OKTA_CLIENT_SECRET,
+  appBaseUrl: 'http://localhost:3000',
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: 'openid profile',
+  routes: {
+    login: {
+      path: '/users/login',
+    },
+    callback: {
+      path: '/users/callback',
+      defaultRedirect: '/locomotives',
+    },
+  },
+});
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 });
 
 app.use('/', api);
 
-app.use(express.static(path.join('dist')));
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401);
+  }
+  return next();
+}
+
+// app.use(express.static(path.join('dist')));
+app.use(markoExpress()); // enable res.marko(template, data)
+
+const template = require('./template.marko');
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../../dist/index.html'));
+  res.marko(template);
 });
+
+/* app.get('*', (req, res, next) => {
+  loginRequired(req, res, next);
+  res.sendFile(path.join(__dirname + '/../../dist/index.html'));
+}); */
 
 /* istanbul ignore next */
 const appPort = process.env.PORT || 5000;
 const server = app.listen(environment === 'test' ? 0 : appPort, () => {});
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: false,
+    secret: process.env.AUTH_STRING,
+  }),
+);
+app.use(oidc.router);
+
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  oktaClient
+    .getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = server;
