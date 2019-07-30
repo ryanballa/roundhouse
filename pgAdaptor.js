049a646d23530004fa450d54348243d3c9f545d2
Ryan Balla <rballa@signal.co>
2019-07-30 15:25:46 -0500

    Conditional test db config

--- pgAdaptor.js
+++ pgAdaptor.js
@@ -6,11 +6,15 @@ const pgp = pgPromise({});
 const config = {
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
-  // password: process.env.POSTGRES_PASSWORD,
+  password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT,
   user: process.env.POSTGRES_USER,
 };
 
-const db = pgp(config);
+const db = pgp(
+  process.env.NODE_ENV === 'test'
+    ? 'postgres://localhost/testroundhouse'
+    : config,
+);
 
 exports.db = db;
