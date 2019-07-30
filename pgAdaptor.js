25d507005f44a7373d4456d261d3d0d26d2ca8e3
Ryan Balla <rballa@signal.co>
2019-07-30 15:19:27 -0500

    Remove pasword for DB

--- pgAdaptor.js
+++ pgAdaptor.js
@@ -6,7 +6,7 @@ const pgp = pgPromise({});
 const config = {
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
-  password: process.env.POSTGRES_PASSWORD,
+  // password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT,
   user: process.env.POSTGRES_USER,
 };
