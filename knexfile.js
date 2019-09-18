30ae57118b0ef227066dae15bbf35a2e0a2deed6
Ryan Balla <rballa@signal.co>
2019-09-18 10:20:25 -0500

    Update test DB connection

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,13 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/dev-roundhouse',
+    connection: {
+      port: process.env.POSTGRES_PORT,
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
     migrations: {
       directory: './db/migrations',
     },
