e41f6794863ad184d323b69014abd2ddff1255c8
Ryan Balla <ryan@ryanballa.com>
2019-11-10 11:52:49 -0600

    Adding icon and display styles for work_items notes

--- src/client/workOrders/WorkOrdersView.js
+++ src/client/workOrders/WorkOrdersView.js
@@ -53,6 +53,18 @@ const StyledDestWrapper = styledComponent('div', {
 });
 
 const StyledUl = styledComponent('ul', {
+  '& .instructions': {
+    '& svg': {
+      fill: '#E67D19',
+      height: '50px',
+      marginLeft: '-20px',
+      marginRight: '10px',
+      overflow: 'hidden',
+      width: '60px',
+    },
+    alignItems: 'center',
+    display: 'flex',
+  },
   '& .task': {
     alignItems: 'center',
     display: 'flex',
@@ -76,6 +88,9 @@ const StyledUl = styledComponent('ul', {
     margin: '10px 0',
   },
   '@media print': {
+    '& .instructions': {
+      fontSize: '130%',
+    },
     '& .task': {
       fontSize: '130%',
     },
@@ -215,7 +230,7 @@ const WorkOrdersView = ({ history, match }) => {
                     }}
                     to={`/work-orders/${workOrder.workOrdersResults[0].id}`}
                   >
-                  Edit
+                    Edit
                   </Link>
                   {!workItem.tasks.length && (
                     <DeleteWorkItem
@@ -227,7 +242,29 @@ const WorkOrdersView = ({ history, match }) => {
                   )}
                 </div>
                 <p>Scheduled work at {workItem.destinationname}</p>
-                {!workItem.tasks.length && <small>No Scheduled Work</small>}
+                {workItem.notes && (
+                  <p className="instructions">
+                    <svg
+                      height="40"
+                      version="1.1"
+                      x="-20px"
+                      y="0px"
+                      viewBox="0 0 80 105"
+                      width="70"
+                    >
+                      <g>
+                        <path d="M50,39.2045441c-0.8634262,0-1.7273788,0.0864487-2.5043564,0.3452644C48.9636803,40.2408676,50,41.7089005,50,43.522728   c0,2.4179077-1.8997459,4.3181801-4.3181801,4.3181801c-2.418438,0-4.3181839-1.9002724-4.3181839-4.3181801   C39.9820518,45.3360252,39.2045441,47.5815659,39.2045441,50c0,5.9591103,4.8363457,10.7954559,10.7954559,10.7954559   S60.7954559,55.9591103,60.7954559,50S55.9591103,39.2045441,50,39.2045441z" />
+                        <path d="M2.5,50c0,0,43.1818199,53.4591141,95,0C45.6818199-3.4591119,2.5,50,2.5,50z M50,67.272728   c-9.4997902,0-17.272728-7.7729378-17.272728-17.272728c0-9.5003166,7.7729378-17.272728,17.272728-17.272728   S67.272728,40.4996834,67.272728,50C67.272728,59.4997902,59.4997902,67.272728,50,67.272728z" />
+                      </g>
+                    </svg>
+                    {workItem.notes}
+                  </p>
+                )}
+                {!workItem.tasks.length && (
+                  <div>
+                    <small>No Scheduled Work</small>
+                  </div>
+                )}
                 <ul>
                   {workItem.tasks.map(task => (
                     <li key={shortid.generate()} className="task">
